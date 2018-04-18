0615b5a6da8a81d6466d3e46a41f65de388aa2e7
maijs <raimonds.kalnins@wunder.io>
2018-04-18 12:50:58 +0300

    Module "migrate_movies" added and enabled.

---
+++ drupal/web/modules/custom/migrate_movies/src/Plugin/migrate/source/MigrateMoviesCSV.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\migrate_movies\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\Row;
+use Drupal\migrate_source_csv\Plugin\migrate\source\CSV as MigrateSourceCSV;
+
+/**
+ * Movies base migration.
+ *
+ * @MigrateSource(
+ *   id = "migrate_movies_csv"
+ * )
+ */
+class MigrateMoviesCSV extends MigrateSourceCSV {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    $module_path = drupal_get_path('module', 'migrate_movies');
+
+    $configuration['path'] = $module_path . '/source/' . $configuration['path'];
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $keywords = json_decode($row->getSourceProperty('keywords'));
+    $keywords = array_map(function($item) {
+      return $item->name;
+    }, $keywords);
+
+    $row->setSourceProperty('keywords', $keywords);
+    return parent::prepareRow($row);
+  }
+
+}
